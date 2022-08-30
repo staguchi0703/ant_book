8a8d9899065636a1b4a5b2d0127d3cdebc5e431e
Sho Taguchi <taguchi.sho@mail.canon>
2022-08-30 13:13:14 +0900

    1111

--- p37_maze_bfs/a_main.py
+++ p37_maze_bfs/a_main.py
@@ -22,40 +22,52 @@ from pprint import pprint
 import numpy as np
 from collections import deque
 N, M = [int(i) for i in input().split()]
-mase = [[i for i in input()] for _ in range(N)]
+maze = [[i for i in input()] for _ in range(N)]
 d = [[np.inf for _ in range(M)] for _ in range(N)]
-q = deque([])
 
+
+s_pos = []
+is_s = False
 for y in range(N):
     for x in range(M):
-        if mase[y][x] == "S":
+        if maze[y][x] == "S":
             s_pos = [x, y]
+            is_s = True
+        if is_s:
+            break
+    if is_s:
+        break
 
 
+
+g_pos = []
+is_g = False
 for y in range(N):
     for x in range(M):
-        if mase[y][x] == "G":
+        if maze[y][x] == "G":
             g_pos = [x, y]
+            is_g = True
+        if is_g:
+            break
+    if is_g:
+        break
 
 print(s_pos, g_pos)
 
 def bfs(s_pos):
-    q.append(s_pos)
+    q = deque([s_pos]) # s_posを一単位として初期化するため[]で囲う
     d[s_pos[1]][s_pos[0]] = 0
 
     while len(q) > 0:
         pos = q.popleft()
-        if pos == g_pos:
-            break
-        else:
-            for dx, dy in [(1,0), (0,1), (-1,0), (0,-1)]:
-                nx, ny = pos[0] + dx, pos[1] + dy
-                if (0<=nx<=M-1) and (0<=ny<=N-1):
-                    if mase[ny][nx] != "#" and d[ny][nx] == np.inf:
-                        d[ny][nx] = d[pos[1]][pos[0]] + 1
-                        q.append([nx, ny])
+        for dx, dy in [(1,0), (0,1), (-1,0), (0,-1)]:
+            nx, ny = pos[0] + dx, pos[1] + dy
+            if (0<=nx<=M-1) and (0<=ny<=N-1):
+                if maze[ny][nx] != "#" and d[ny][nx] == np.inf:
+                    d[ny][nx] = d[pos[1]][pos[0]] + 1
+                    q.append([nx, ny])
     return d
 
 d = bfs(s_pos)
-pprint(d)
+pprint(np.array(d))
 print(d[g_pos[1]][g_pos[0]])
