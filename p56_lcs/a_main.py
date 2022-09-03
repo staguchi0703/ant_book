307e9abcc799a2f3fdc9782b16e6ce170915b6d3
Sho Taguchi <s.taguchi703@gmail.com>
2022-09-03 10:43:02 +0900

    lsc

--- p56_lcs/a_main.py
+++ p56_lcs/a_main.py
@@ -23,3 +23,17 @@ from pprint import pprint
 N, M = [int(i) for i in input().split()]
 S = input()
 T = input()
+
+dp = [[0 for _ in range(M+1)] for _ in range(N+1)]
+
+for i in range(N):
+    for j in range(M):
+        if S[i] == T[j]:
+            dp[i+1][j+1] = dp[i][j] +1
+        else:
+            dp[i+1][j+1] = max(dp[i+1][j], dp[i][j+1])
+
+    pprint(dp)
+    print("-"*20)
+
+print(dp[N][M]) 
